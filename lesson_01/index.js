2c93d74224896d8c48492b1b7f89e29ecb0df024
Irek Yakubov <irekyyakubov@gmail.com>
2022-04-28 21:41:40 +0400

    дабавление второго товара

--- lesson_01/index.js
+++ lesson_01/index.js
@@ -1,7 +1,20 @@
-const smartPhoneName = 'Sony';
-const quantityGoods = 3;
-const categoryGoods = 'Phone';
-const priceGoods = 1200;
+'use strict'
+{
+    const smartPhoneName = 'Sony';
+    const quantityGoods = 3;
+    const categoryGoods = 'Phone';
+    const priceGoods = 1200;
 
-console.log('Наименование товара:', smartPhoneName);
-console.log('Общая сумма товаров:', priceGoods * quantityGoods, '$');
+    console.log('Наименование товара:', smartPhoneName);
+    console.log('Общая сумма товаров:', priceGoods * quantityGoods, '$');
+}
+
+{
+    const smartPhoneName = 'Iphone 11';
+    const quantityGoods = 4;
+    const categoryGoods = 'Phone';
+    const priceGoods = 1800;
+
+    console.log('Наименование товара:', smartPhoneName);
+    console.log('Общая сумма товаров:', priceGoods * quantityGoods, '$');
+}
