4c55c4ce70b8cde21ead92c6b78a49b802ca1c39
Irek Yakubov <irekyyakubov@gmail.com>
2022-05-27 23:41:05 +0400

    Задача урока 13

---
+++ lesson_13/game.js
@@ -0,0 +1,96 @@
+'use strict';
+(() => {
+  const FIGURES_ENG = ['rock', 'scissors', 'paper'];
+  const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
+
+  const getRandomIntInclusive = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const game = (language) => {
+    const result = {
+      player: 0,
+      computer: 0,
+    };
+    const lang = language === 'EN' || language === 'ENG' ?
+      FIGURES_ENG : FIGURES_RUS;
+    return function start() {
+      const stepComputer = getRandomIntInclusive(0, FIGURES_RUS.length);
+      const сhoiceMan = prompt(lang.toString());
+      const choiceComp = lang[stepComputer];
+      const rock = lang[0][0];
+      const scissors = lang[1][0];
+      const paper = lang[2][0];
+      if (сhoiceMan === null) {
+        const exitGame = prompt('точно ли хотите выйти?');
+        if (exitGame !== null) {
+          if (language === 'EN' || language === 'ENG') {
+            return alert(`Game result:
+            Computer: ${result.computer};
+            Player: ${result.player}`);
+          } else {
+            return alert(`Результат игр:
+            Компьютер: ${result.computer};
+            Игрок: ${result.player}`);
+          }
+        } else {
+          return start();
+        }
+      }
+
+      if (сhoiceMan === '') {
+        alert('введите свой ход');
+      }
+
+      if (сhoiceMan[0] === choiceComp[0]) {
+        alert('Ничья');
+      } else {
+        if (сhoiceMan[0] === rock) {
+          if (choiceComp[0] === scissors) {
+            alert(`Компьютер: ${choiceComp}
+            Вы: ${lang[0]}
+            Вы выиграли!`);
+            result.player += 1;
+          } else {
+            alert(`Компьютер: ${choiceComp}
+            Вы: ${lang[0]}
+            Вы проиграли!`);
+            result.computer += 1;
+          }
+        }
+
+        if (сhoiceMan[0] === scissors) {
+          if (choiceComp[0] === rock) {
+            alert(`Компьютер: ${choiceComp}
+            Вы: ${lang[1]}
+            Вы проиграли!`);
+            result.computer += 1;
+          } else {
+            alert(`Компьютер: ${choiceComp}
+            Вы: ${lang[1]}
+            Вы выиграли!`);
+            result.player += 1;
+          }
+        }
+
+        if (сhoiceMan[0] === paper) {
+          if (choiceComp[0] === rock) {
+            alert(`Компьютер: ${choiceComp}
+            Вы: ${lang[2]}
+            Вы выиграли!`);
+            result.player += 1;
+          } else {
+            alert(`Компьютер: ${choiceComp}
+            Вы: ${lang[2]}
+            Вы проиграли!`);
+            result.computer += 1;
+          }
+        }
+      }
+      return start();
+    };
+  };
+  window.RPS = game;
+})();
